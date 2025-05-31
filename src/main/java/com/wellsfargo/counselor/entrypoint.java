d31b88261eb12dd4084de443f86f96f8a9ab07bc
Srikasula <srikanthgoudk3@gmail.com>
2025-05-31 14:01:46 -0500

    Added Client, Portfolio, Security entity classes

--- src/main/java/com/wellsfargo/counselor/entrypoint.java
+++ src/main/java/com/wellsfargo/counselor/entrypoint.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class Entrypoint {
+public class entrypoint {
 
     public static void main(String[] args) {
-        SpringApplication.run(Entrypoint.class, args);
+        SpringApplication.run(entrypoint.class, args);
     }
 
 }
